341b57e609ac6bc6c3c26cfa26e7377e717ea159
Justin <jds@justindsmith.ca>
2024-09-16 20:47:49 -0400

    updated TextInputs to be able to delete inputs but need to delete the pulled up tasks and thanks #fail#debugged

--- Form_Components/Form_Parts/xTextInput.js
+++ Form_Components/Form_Parts/xTextInput.js
@@ -1,31 +1,64 @@
 
 import { TextInput, View, Text, StyleSheet, TouchableOpacity } from 'react-native';
-import React from 'react';
+import React, { useState } from 'react';
 
 const styles = StyleSheet.create({
   input:{
+    color: '#fff',
+    textAlignVertical: 'top',
+    fontFamily: 'ds-digi',
+    fontSize: 20,
+    padding: 10,
+  },
+  deleteInput:{
+    color: 'black',
+    textAlignVertical: 'top',
+    fontFamily: 'ds-digi',
+    fontSize: 20,
+    padding: 10,
+  },
+  touch:{
     position: 'relative',
     flexDirection: 'row',
     borderWidth: 2,
     borderColor: '#fff',
-    color: '#fff',
-    textAlignVertical: 'top',
-    padding: 10,
-    fontFamily: 'ds-digi',
     margin: 2,
-  }
+  },
+  deleteTouch:{
+    position: 'relative',
+    flexDirection: 'row',
+    borderWidth: 2,
+    borderColor: '#fff',
+    margin: 2,
+    backgroundColor: 'red'
+  },
   });
 
+
+
 const XTextInput = (props) => {
+  const [deleteActivated, setDeleteActivated] = useState(false)
+
+    const toggleDelete = () => {
+      setDeleteActivated(!deleteActivated);
+    }
     return (
-        <TextInput
-        height = {props.height}
-        style={[styles.input, {height : props.height, width: props.width, flex: props.flex}]}
-        value={props.description}
-        multiline={true}
-        onChangeText={props.setDescription} // Use the setter function passed as a prop
-        placeholder = {props.placeholder}
-        />
+      <TouchableOpacity style={[deleteActivated ? styles.deleteTouch: styles.touch, 
+      {height : props.height, width: props.width, flex: props.flex}]}
+      onLongPress={toggleDelete}>
+        {deleteActivated ? ( 
+        <Text style = {styles.deleteInput}>DELETE</Text>
+        ):(
+          <TextInput
+          style = {styles.input}
+          value={props.description}
+          multiline={true}
+          onChangeText={props.setDescription} // Use the setter function passed as a prop
+          placeholder = {props.placeholder}
+          />
+        )
+      }
+      </TouchableOpacity>
     );
   };
 
